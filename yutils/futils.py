5883dade9739541aa30380bce9d61d91d13cbbb4
HAESEND <dominique.haesen@vito.be>
2019-04-09 15:18:48 +0200

    tinkering with files

--- yutils/futils.py
+++ yutils/futils.py
@@ -9,7 +9,7 @@ import logging
 #
 #
 #
-def sanePath(szabspath):
+def sanePath(szabspath, verbose = True):
     """
     attempt to check whether path is representing a meaningful path,
     without automatic joining of current working directory,
@@ -18,252 +18,255 @@ def sanePath(szabspath):
     knowing that this module will cause far more problems than it possibly could solve. Ha!
     """
     if not os.path.isabs(szabspath):
-        logging.warn("Non absolute path: %s", repr(szabspath))
+        if verbose: logging.warning("Non absolute path: %s", repr(szabspath))
         return None
     szabsrealpath = os.path.abspath(os.path.realpath(szabspath))
-    logging.debug("Valid path: %s - using: %s", repr(szabspath), repr(szabsrealpath))
+    if verbose: logging.debug("Valid path: %s - using: %s", repr(szabspath), repr(szabsrealpath))
     return szabsrealpath
 
 #
 #
 #
-def saneExistingPath(szabspath):
+def saneExistingPath(szabspath, verbose = True):
     """
     attempt to check whether path is a string representing a meaningful existing path,
     and convert it to its no nonsense form
     """
-    szsanepath = sanePath(szabspath)
+    szsanepath = sanePath(szabspath, verbose=verbose)
     if not szsanepath: 
-        logging.warn("Invalid absolute path: %s", repr(szabspath))
+        if verbose: logging.warning("Invalid absolute path: %s", repr(szabspath))
         return None
     if not os.path.exists(szsanepath):
-        logging.warn("Path not found: %s", repr(szabspath))
+        if verbose: logging.warning("Path not found: %s", repr(szabspath))
         return None
-    logging.debug("Valid existing path: %s - using: %s", repr(szabspath), repr(szsanepath))
+    if verbose: logging.debug("Valid existing path: %s - using: %s", repr(szabspath), repr(szsanepath))
     return szsanepath
 
 #
 #
 #
-def saneExistingDirectory(szabspath):
+def saneExistingDirectory(szabspath, verbose = True):
     """
     attempt to check whether path is a string representing a meaningful existing directory,
     and convert it to its no nonsense form
     """
-    szsanepath = saneExistingPath(szabspath)
+    szsanepath = saneExistingPath(szabspath, verbose=verbose)
     if not szsanepath: 
-        logging.warn("Invalid absolute path: %s", repr(szabspath))
+        if verbose: logging.warning("Invalid absolute path: %s", repr(szabspath))
         return None
     if not os.path.isdir(szsanepath):
-        logging.warn("Non directory path %s", repr(szabspath))
+        if verbose: logging.warning("Non directory path %s", repr(szabspath))
         return None
-    logging.debug("Valid existing directory path: %s - using: %s", repr(szabspath), repr(szsanepath))
+    if verbose: logging.debug("Valid existing directory path: %s - using: %s", repr(szabspath), repr(szsanepath))
     return szsanepath
 
 #
 #
 #
-def saneExistingFile(szabspath):
+def saneExistingFile(szabspath, verbose = True):
     """
     attempt to check whether path is a string representing a meaningful existing file,
     and convert it to its no nonsense form
     """
-    szsanepath = saneExistingPath(szabspath)
+    szsanepath = saneExistingPath(szabspath, verbose=verbose)
     if not szsanepath: 
-        logging.warn("Invalid absolute path: %s", repr(szabspath))
+        if verbose: logging.warning("Invalid absolute path: %s", repr(szabspath))
         return None
     if not os.path.isfile(szsanepath):
-        logging.warn("Non file path %s", repr(szabspath))
+        if verbose: logging.warning("Non file path %s", repr(szabspath))
         return None
-    logging.debug("Valid existing file path: %s - using: %s", repr(szabspath), repr(szsanepath))
+    if verbose: logging.debug("Valid existing file path: %s - using: %s", repr(szabspath), repr(szsanepath))
     return szsanepath
 
 #
 #
 #
-def saneFile(szabspath, bmayexist = False):
+def saneFile(szabspath, bmayexist = False, verbose = True):
     """
     check whether path is a string representing a meaningful non-existing file,
     check whether the directory it should live in exists
     convert it to its no nonsense form
     """
-    szsanepath = sanePath(szabspath)
+    szsanepath = sanePath(szabspath, verbose=verbose)
     if not szsanepath: 
-        logging.warn("Invalid absolute path: %s", repr(szabspath))
+        if verbose: logging.warning("Invalid absolute path: %s", repr(szabspath))
         return None
     if os.path.isdir(szsanepath):
-        logging.warn("Existing directory path %s", repr(szabspath))
+        if verbose: logging.warning("Existing directory path %s", repr(szabspath))
         return None
     if not bmayexist:
         if os.path.isfile(szsanepath):
-            logging.warn("Existing file path %s", repr(szabspath))
+            if verbose: logging.warning("Existing file path %s", repr(szabspath))
             return None
-    
+
     szabsdirpath = os.path.dirname(szsanepath)
     if not os.path.isdir(szabsdirpath):
-        logging.warn("Non directory path %s", repr(szabspath))
+        if verbose: logging.warning("Non directory path %s", repr(szabspath))
         return None
-    
-    logging.debug("Valid file path: %s - using: %s", repr(szabspath), repr(szsanepath))
+
+    if verbose: logging.debug("Valid file path: %s - using: %s", repr(szabspath), repr(szsanepath))
     return szsanepath
 
 #
 #
 #
-def directoryFromExistingFile(szabspath):
+def directoryFromExistingFile(szabspath, verbose = True):
     """
     """
-    szsanepath = saneExistingFile(szabspath)
+    szsanepath = saneExistingFile(szabspath, verbose=verbose)
     if not szsanepath:
-        logging.warn("Non file path %s", repr(szabspath))
+        if verbose: logging.warning("Non file path %s", repr(szabspath))
         return None
     szabsdirpath = os.path.dirname(szsanepath)
+    if verbose: logging.debug("Valid directory path from existing file: %s - using: %s", repr(szabspath), repr(szabsdirpath))
     return szabsdirpath
 
 #
 #
 #
-def directoryFromFile(szabspath):
+def directoryFromFile(szabspath, verbose = True):
     """
     """
-    szsanepath = saneFile(szabspath, True)
+    szsanepath = saneFile(szabspath, bmayexist=True, verbose=verbose)
     if not szsanepath:
-        logging.warn("Non file path %s", repr(szabspath))
+        if verbose: logging.warning("Non file path %s", repr(szabspath))
         return None
     szabsdirpath = os.path.dirname(szsanepath)
+    if verbose: logging.debug("Valid directory path from file: %s - using: %s", repr(szabspath), repr(szabsdirpath))
     return szabsdirpath
 
 #
 #
 #
-def parentFromExistingDirectory(szabspath):
+def parentFromExistingDirectory(szabspath, verbose = True):
     """
     """
-    szsanepath = saneExistingDirectory(szabspath)
+    szsanepath = saneExistingDirectory(szabspath, verbose=verbose)
     if not szsanepath:
-        logging.warn("Non directory path %s", repr(szabspath))
+        if verbose: logging.warning("Non directory path %s", repr(szabspath))
         return None
-    szsanepath = saneExistingDirectory(os.path.dirname(szabspath))
+    szsanepath = saneExistingDirectory(os.path.dirname(szabspath), verbose=verbose)
     if not szsanepath:
-        logging.warn("Directory path %s has no parent", repr(szabspath))
+        if verbose: logging.warning("Directory path %s has no parent", repr(szabspath))
         return None
+    if verbose: logging.debug("Directory parent path from existing directory: %s - using: %s", repr(szabspath), repr(szsanepath))
     return szsanepath
 
 # 
 #
 #
-def gFilesInDir(szabsdir):
+def gFilesInDir(szabsdir, verbose = True):
     """
     generator yielding the files in a directory
     """
-    szsanedirpath = saneExistingDirectory(szabsdir)
+    szsanedirpath = saneExistingDirectory(szabsdir, verbose=verbose)
     if not szsanedirpath:
-        logging.warn("Invalid absolute directory: %s", repr(szabsdir))
+        if verbose: logging.warning("Invalid absolute directory: %s", repr(szabsdir))
         return
     listofitemsindir = os.listdir(szsanedirpath)
     for stringitem in listofitemsindir:
-        saneexistingpath = saneExistingPath(os.path.join(szsanedirpath, stringitem))
+        saneexistingpath = saneExistingPath(os.path.join(szsanedirpath, stringitem), verbose=verbose)
         if saneexistingpath:
             if os.path.isfile(saneexistingpath):
-                logging.debug("Using file: %s", repr(saneexistingpath))
+                if verbose: logging.debug("Using file: %s", repr(saneexistingpath))
                 yield saneexistingpath
             else:
-                logging.debug("Ignoring non-file path: %s", repr(saneexistingpath))
+                if verbose: logging.debug("Ignoring non-file path: %s", repr(saneexistingpath))
 
 # 
 #
 #
-def gfFilesInDir(predicate, szabsdir):
+def gfFilesInDir(predicate, szabsdir, verbose = True):
     """
     generator yielding the files, for which the predicate is True, from a directory
     """
-    for szfilepath in filter(predicate, gFilesInDir(szabsdir)):
-        logging.debug("Using file: %s", repr(szfilepath))
+    for szfilepath in filter(predicate, gFilesInDir(szabsdir, verbose=verbose)):
+        if verbose: logging.debug("Using file: %s", repr(szfilepath))
         yield szfilepath
 
 #
 #
 #
-def ggFilesInDirs(iszabsdirs):
+def ggFilesInDirs(itrszabsdirs, verbose = True):
     """
-    generator yielding the files from a directories iterable
+    generator yielding the files from a directories iterable. beware: nothing prevents lstszabsdirs to contain duplicates.
     """
-    if not iszabsdirs:
-        logging.warn("Invalid directories iterable: %s", repr(iszabsdirs))
+    if not itrszabsdirs:
+        if verbose: logging.warning("Invalid directories iterable: %s", repr(itrszabsdirs))
         return
-    for iszabsdir in iszabsdirs:
-        for szfilepath in gFilesInDir(iszabsdir):
-            logging.debug("Using file: %s", repr(szfilepath))
+    for szabsdir in itrszabsdirs:
+        for szfilepath in gFilesInDir(szabsdir, verbose=verbose):
+            if verbose: logging.debug("Using file: %s", repr(szfilepath))
             yield szfilepath
 
 # 
 #
 #
-def ggfFilesInDirs(predicate, iszabsdirs):
+def ggfFilesInDirs(predicate, itrszabsdirs, verbose = True):
     """
     generator yielding the files, for which the predicate is True, from a directories iterable
     """
-    for szfilepath in filter(predicate, ggFilesInDirs(iszabsdirs)):
-        logging.debug("Using file: %s", repr(szfilepath))
+    for szfilepath in filter(predicate, ggFilesInDirs(itrszabsdirs, verbose=verbose)):
+        if verbose: logging.debug("Using file: %s", repr(szfilepath))
         yield szfilepath
 
 # 
 #
 #
-def gDirsInDir(szabsdir):
+def gDirsInDir(szabsdir, verbose = True):
     """
     generator yielding the subdirectories in a directory
     """
-    szsanedirpath = saneExistingDirectory(szabsdir)
+    szsanedirpath = saneExistingDirectory(szabsdir, verbose=verbose)
     if not szsanedirpath:
-        logging.warn("Invalid absolute directory: %s", repr(szabsdir))
+        if verbose: logging.warning("Invalid absolute directory: %s", repr(szabsdir))
         return
     listofitemsindir = os.listdir(szsanedirpath)
     for stringitem in listofitemsindir:
-        saneexistingpath = saneExistingPath(os.path.join(szsanedirpath, stringitem))
+        saneexistingpath = saneExistingPath(os.path.join(szsanedirpath, stringitem), verbose=verbose)
         if saneexistingpath:
             if os.path.isdir(saneexistingpath):
-                logging.debug("Using directory: %s", repr(saneexistingpath))
+                if verbose: logging.debug("Using directory: %s", repr(saneexistingpath))
                 yield saneexistingpath
             else:
-                logging.debug("Ignoring non-directory path: %s", repr(saneexistingpath))
+                if verbose: logging.debug("Ignoring non-directory path: %s", repr(saneexistingpath))
                 
 
 # 
 #
 #
-def gfDirsInDir(predicate, szabsdir):
+def gfDirsInDir(predicate, szabsdir, verbose = True):
     """
     generator yielding the subdirectories, for which the predicate is True, from a directory
     """
-    for szdirpath in filter(predicate, gDirsInDir(szabsdir)):
-        logging.debug("Using directory: %s", repr(szdirpath))
+    for szdirpath in filter(predicate, gDirsInDir(szabsdir, verbose=verbose)):
+        if verbose: logging.debug("Using directory: %s", repr(szdirpath))
         yield szdirpath
 
 #
 #
 #
-def ggDirsInDirs(iszabsdirs):
+def ggDirsInDirs(itrszabsdirs, verbose = True):
     """
     generator yielding the subdirectories from a directories iterable
     """
-    if not iszabsdirs:
-        logging.warn("Invalid directories iterable: %s", repr(iszabsdirs))
+    if not itrszabsdirs:
+        if verbose: logging.warning("Invalid directories iterable: %s", repr(itrszabsdirs))
         return
-    for iszabsdirs in iszabsdirs:
-        for szdirpath in gDirsInDir(iszabsdirs):
-            logging.debug("Using directory: %s", repr(szdirpath))
+    for szabsdir in itrszabsdirs:
+        for szdirpath in gDirsInDir(szabsdir, verbose=verbose):
+            if verbose: logging.debug("Using directory: %s", repr(szdirpath))
             yield szdirpath
 
 # 
 #
 #
-def ggfDirsInDirs(predicate, iszabsdirs):
+def ggfDirsInDirs(predicate, itrszabsdirs, verbose = True):
     """
     generator yielding the subdirectories, for which the predicate is True, from a directories iterable
     """
-    for szdirpath in filter(predicate, ggDirsInDirs(iszabsdirs)):
-        logging.debug("Using directory: %s", repr(szdirpath))
+    for szdirpath in filter(predicate, ggDirsInDirs(itrszabsdirs, verbose)):
+        if verbose: logging.debug("Using directory: %s", repr(szdirpath))
         yield szdirpath
 
 # 
@@ -275,7 +278,7 @@ def gFilesInDirTree(szabsdir):
     """
     szsanedirpath = saneExistingDirectory(szabsdir)
     if not szsanedirpath:
-        logging.warn("Invalid abs directory: %s", repr(szabsdir))
+        logging.warning("Invalid abs directory: %s", repr(szabsdir))
         return
     listofitemsindir = os.listdir(szsanedirpath)
     for stringitem in listofitemsindir:
@@ -286,6 +289,6 @@ def gFilesInDirTree(szabsdir):
             for item in gFilesInDirTree(saneexistingpath):
                 yield item
         else:
-            logging.warn("Directory: %s contains alien entry: %s", repr(szabsdir), repr(saneexistingpath))
+            logging.warning("Directory: %s contains alien entry: %s", repr(szabsdir), repr(saneexistingpath))
             return
 
---
+++ yutils/test_futils.py
@@ -0,0 +1,193 @@
+#
+#
+#
+import unittest
+import logging
+import os
+import shutil
+import tempfile
+import yutils.futils
+
+#
+#
+#
+verbose = False
+level   = logging.DEBUG #logging.ERROR #logging.DEBUG #logging.ERROR #
+
+#
+#
+#
+class Test(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+
+        cls.sz_test_rootdir   = tempfile.mkdtemp()
+        cls.sz_some_dir_name  = 'somedir'
+        cls.sz_othr_dir_name  = 'othrdir'
+        cls.sz_none_dir_name  = 'nonedir'
+        cls.sz_some_file_name = 'somefile.txt'
+        cls.sz_none_file_name = 'nonefile.txt'
+
+        cls.sz_some_dir       = os.path.join(cls.sz_test_rootdir, cls.sz_some_dir_name)
+        cls.sz_othr_dir       = os.path.join(cls.sz_test_rootdir, cls.sz_othr_dir_name)
+        cls.sz_none_dir       = os.path.join(cls.sz_test_rootdir, cls.sz_none_dir_name)
+        cls.sz_some_file      = os.path.join(cls.sz_some_dir, cls.sz_some_file_name)
+        cls.sz_none_file      = os.path.join(cls.sz_some_dir, cls.sz_none_file_name)
+
+        os.mkdir(cls.sz_some_dir)
+        os.mkdir(cls.sz_othr_dir)
+
+        def makefile(szfile):
+            f = open(szfile, 'w')
+            f.write(szfile)
+            f.write('\nThe owls are not what they seem')
+            f.close()
+
+        makefile(cls.sz_some_file)
+
+        for szyyyymmdd in ['20170101', '20170102', '20170103', '20171231']:
+            makefile(os.path.join(cls.sz_some_dir, 'otherfile_' + szyyyymmdd + '_someversion.txt'))
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls.sz_test_rootdir)
+        pass
+
+    #@unittest.case.skip
+    def test_sanePath(self):
+        #
+        #    sanePath
+        #
+        self.assertIsNotNone(yutils.futils.sanePath(Test.sz_some_dir,  verbose=verbose))
+        self.assertIsNotNone(yutils.futils.sanePath(Test.sz_none_dir,  verbose=verbose))
+        self.assertIsNotNone(yutils.futils.sanePath(Test.sz_some_file, verbose=verbose))
+        self.assertIsNotNone(yutils.futils.sanePath(Test.sz_none_file, verbose=verbose))
+        #
+        #    saneExistingPath
+        #
+        self.assertIsNotNone(yutils.futils.saneExistingPath(Test.sz_some_dir,  verbose=verbose))
+        self.assertIsNotNone(yutils.futils.saneExistingPath(Test.sz_some_file, verbose=verbose))
+
+        self.assertIsNone(yutils.futils.saneExistingPath(Test.sz_none_dir,  verbose=verbose))
+        self.assertIsNone(yutils.futils.saneExistingPath(Test.sz_none_file, verbose=verbose))
+        #
+        #    saneExistingDirectory
+        #
+        self.assertIsNotNone(yutils.futils.saneExistingDirectory(Test.sz_some_dir, verbose=verbose))
+
+        self.assertIsNone(yutils.futils.saneExistingDirectory(Test.sz_none_dir,  verbose=verbose))
+        self.assertIsNone(yutils.futils.saneExistingDirectory(Test.sz_some_file, verbose=verbose))
+        self.assertIsNone(yutils.futils.saneExistingDirectory(Test.sz_none_file, verbose=verbose))
+        #
+        #    saneExistingFile
+        #
+        self.assertIsNotNone(yutils.futils.saneExistingFile(Test.sz_some_file, verbose=verbose))
+
+        self.assertIsNone(yutils.futils.saneExistingFile(Test.sz_some_dir,  verbose=verbose))
+        self.assertIsNone(yutils.futils.saneExistingFile(Test.sz_none_dir,  verbose=verbose))
+        self.assertIsNone(yutils.futils.saneExistingFile(Test.sz_none_file, verbose=verbose))
+        #
+        #    saneFile
+        #
+        self.assertIsNotNone(yutils.futils.saneFile(Test.sz_some_file, bmayexist=True, verbose=verbose))
+        self.assertIsNone(yutils.futils.saneFile(Test.sz_some_file, bmayexist=False,   verbose=verbose))
+
+        self.assertIsNone(yutils.futils.saneFile(Test.sz_some_dir, bmayexist=True,  verbose=verbose))
+        self.assertIsNone(yutils.futils.saneFile(Test.sz_some_dir, bmayexist=False, verbose=verbose))
+
+        self.assertIsNotNone(yutils.futils.saneFile(Test.sz_none_file, bmayexist=True,  verbose=verbose))
+        self.assertIsNotNone(yutils.futils.saneFile(Test.sz_none_file, bmayexist=False, verbose=verbose))
+
+        self.assertIsNotNone(yutils.futils.saneFile(Test.sz_none_dir, bmayexist=True, verbose=verbose))
+        self.assertIsNotNone(yutils.futils.saneFile(Test.sz_none_dir, bmayexist=False, verbose=verbose))
+        #
+        #    directoryFromExistingFile
+        #
+        self.assertIsNotNone(yutils.futils.directoryFromExistingFile(Test.sz_some_file, verbose=verbose))
+
+        self.assertIsNone(yutils.futils.directoryFromExistingFile(Test.sz_some_dir,  verbose=verbose))
+        self.assertIsNone(yutils.futils.directoryFromExistingFile(Test.sz_none_dir,  verbose=verbose))
+        self.assertIsNone(yutils.futils.directoryFromExistingFile(Test.sz_none_file, verbose=verbose))
+        #
+        #    directoryFromFile
+        #
+        self.assertIsNotNone(yutils.futils.directoryFromFile(Test.sz_some_file, verbose=verbose))
+        self.assertIsNotNone(yutils.futils.directoryFromFile(Test.sz_none_file, verbose=verbose))
+        self.assertIsNotNone(yutils.futils.directoryFromFile(Test.sz_none_dir,  verbose=verbose)) # non existent directory cannot be distinguished from non existent file
+
+        self.assertIsNone(yutils.futils.directoryFromFile(Test.sz_some_dir,  verbose=verbose))
+        #
+        #    parentFromExistingDirectory
+        #
+        self.assertIsNotNone(yutils.futils.parentFromExistingDirectory(Test.sz_some_dir,  verbose=verbose))
+
+        self.assertIsNone(yutils.futils.parentFromExistingDirectory(Test.sz_some_file, verbose=verbose))
+        self.assertIsNone(yutils.futils.parentFromExistingDirectory(Test.sz_none_dir,  verbose=verbose))
+        self.assertIsNone(yutils.futils.parentFromExistingDirectory(Test.sz_none_file, verbose=verbose))
+
+    #@unittest.case.skip
+    def test_gFilesInDir(self):
+        #
+        #    gFilesInDir
+        #
+        self.assertCountEqual( [os.path.basename(p) for p in yutils.futils.gFilesInDir(Test.sz_some_dir,     verbose=verbose)], os.listdir(Test.sz_some_dir))
+        self.assertCountEqual( [os.path.basename(p) for p in yutils.futils.gFilesInDir(Test.sz_none_dir,     verbose=verbose)], [])
+        self.assertCountEqual( [os.path.basename(p) for p in yutils.futils.gFilesInDir(Test.sz_some_file,    verbose=verbose)], [])
+        self.assertCountEqual( [os.path.basename(p) for p in yutils.futils.gFilesInDir(Test.sz_none_file,    verbose=verbose)], [])
+        self.assertCountEqual( [os.path.basename(p) for p in yutils.futils.gFilesInDir(Test.sz_test_rootdir, verbose=verbose)], []) # root contains directories, no files
+        #
+        #    gfFilesInDir
+        #
+        self.assertCountEqual( list(yutils.futils.gfFilesInDir(None,           Test.sz_some_dir, verbose=verbose)), list(yutils.futils.gFilesInDir(Test.sz_some_dir, verbose=verbose)))
+        self.assertCountEqual( list(yutils.futils.gfFilesInDir(lambda _:True,  Test.sz_some_dir, verbose=verbose)), list(yutils.futils.gFilesInDir(Test.sz_some_dir, verbose=verbose)))
+        self.assertCountEqual( list(yutils.futils.gfFilesInDir(lambda _:False, Test.sz_some_dir, verbose=verbose)), [])
+        #
+        #    ggFilesInDirs - 'mylist = list(dict.fromkeys(mylist))' is used to remove duplicates
+        #
+        self.assertCountEqual( list(yutils.futils.ggFilesInDirs([Test.sz_some_dir],                                 verbose=verbose)),  list(yutils.futils.gFilesInDir(Test.sz_some_dir, verbose=verbose)))
+        self.assertCountEqual( list(dict.fromkeys(yutils.futils.ggFilesInDirs([Test.sz_some_dir, Test.sz_some_dir], verbose=verbose))), list(yutils.futils.gFilesInDir(Test.sz_some_dir, verbose=verbose)))
+        #
+        #    ggfFilesInDirs - 'mylist = list(dict.fromkeys(mylist))' is used to remove duplicates
+        #
+        self.assertCountEqual( list(yutils.futils.ggfFilesInDirs(lambda _:True,                [Test.sz_some_dir],                   verbose=verbose)),  list(yutils.futils.gFilesInDir(Test.sz_some_dir, verbose=verbose)))
+        self.assertCountEqual( list(dict.fromkeys(yutils.futils.ggfFilesInDirs(lambda _:True,  [Test.sz_some_dir, Test.sz_some_dir], verbose=verbose))), list(yutils.futils.gFilesInDir(Test.sz_some_dir, verbose=verbose)))
+        self.assertCountEqual( list(dict.fromkeys(yutils.futils.ggfFilesInDirs(lambda _:False, [Test.sz_some_dir, Test.sz_some_dir], verbose=verbose))), [])
+
+
+    #@unittest.case.skip
+    def test_gDirsInDir(self):
+        #
+        #    gDirsInDir
+        #
+        self.assertCountEqual( [os.path.basename(p) for p in yutils.futils.gDirsInDir(Test.sz_test_rootdir,     verbose=verbose)], os.listdir(Test.sz_test_rootdir))
+        self.assertCountEqual( [os.path.basename(p) for p in yutils.futils.gDirsInDir(Test.sz_some_dir,         verbose=verbose)], [])
+        #
+        #    gfDirsInDir
+        #
+        self.assertCountEqual( list(yutils.futils.gfDirsInDir(None,           Test.sz_test_rootdir, verbose=verbose)), list(yutils.futils.gDirsInDir(Test.sz_test_rootdir, verbose=verbose)))
+        self.assertCountEqual( list(yutils.futils.gfDirsInDir(lambda _:True,  Test.sz_test_rootdir, verbose=verbose)), list(yutils.futils.gDirsInDir(Test.sz_test_rootdir, verbose=verbose)))
+        self.assertCountEqual( list(yutils.futils.gfDirsInDir(lambda _:False, Test.sz_test_rootdir, verbose=verbose)), [])
+        #
+        #    ggDirsInDirs
+        #
+        self.assertCountEqual( [os.path.basename(p) for p in yutils.futils.ggDirsInDirs([Test.sz_test_rootdir],     verbose=verbose)], os.listdir(Test.sz_test_rootdir))
+        self.assertCountEqual( [os.path.basename(p) for p in yutils.futils.ggDirsInDirs([Test.sz_some_dir],         verbose=verbose)], [])
+        #
+        #    ggfDirsInDirs
+        #
+        self.assertCountEqual( list(yutils.futils.ggfDirsInDirs(None,           [Test.sz_test_rootdir], verbose=verbose)), list(yutils.futils.gDirsInDir(Test.sz_test_rootdir, verbose=verbose)))
+        self.assertCountEqual( list(yutils.futils.ggfDirsInDirs(lambda _:True,  [Test.sz_test_rootdir], verbose=verbose)), list(yutils.futils.gDirsInDir(Test.sz_test_rootdir, verbose=verbose)))
+        self.assertCountEqual( list(yutils.futils.ggfDirsInDirs(lambda _:False, [Test.sz_test_rootdir], verbose=verbose)), [])
+
+#
+#
+#
+if __name__ == "__main__":
+    #
+    #
+    #
+    logging.basicConfig(level=level, format='%(asctime)s %(levelname).3s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
